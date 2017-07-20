e87c71775a92ba3706421c47181a16f679fed6f7
Tiago Mourao Teixeira <Tiago Mourao Teixeira>
2017-07-20 11:36:22 -0300

    Add project files.

---
+++ miyagi/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace miyagi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox5 = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 219.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(365, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(549, 332);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0.0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 1279F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(52, 1);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1279, 373);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(52, 473);
+            this.maskedTextBox1.Mask = "0.0";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(249, 55);
+            this.maskedTextBox1.TabIndex = 30;
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox1_KeyDown_1);
+            this.maskedTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.maskedTextBox1_KeyPress);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.maskedTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox2.Location = new System.Drawing.Point(309, 473);
+            this.maskedTextBox2.Mask = "0.0";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(249, 55);
+            this.maskedTextBox2.TabIndex = 31;
+            this.maskedTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox2_KeyDown);
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.maskedTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox3.Location = new System.Drawing.Point(567, 473);
+            this.maskedTextBox3.Mask = "0.0";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(249, 55);
+            this.maskedTextBox3.TabIndex = 32;
+            this.maskedTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox3_KeyDown);
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.maskedTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox4.Location = new System.Drawing.Point(824, 473);
+            this.maskedTextBox4.Mask = "0.0";
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(249, 55);
+            this.maskedTextBox4.TabIndex = 33;
+            this.maskedTextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox4_KeyDown);
+            // 
+            // maskedTextBox5
+            // 
+            this.maskedTextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.maskedTextBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox5.Location = new System.Drawing.Point(1082, 473);
+            this.maskedTextBox5.Mask = "0.0";
+            this.maskedTextBox5.Name = "maskedTextBox5";
+            this.maskedTextBox5.Size = new System.Drawing.Size(249, 55);
+            this.maskedTextBox5.TabIndex = 34;
+            this.maskedTextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBox5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox5_KeyDown);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Image = global::miyagi.Properties.Resources.laranja;
+            this.label6.Location = new System.Drawing.Point(1175, 390);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 55);
+            this.label6.TabIndex = 29;
+            this.label6.Text = " ";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Image = global::miyagi.Properties.Resources.Azul;
+            this.label5.Location = new System.Drawing.Point(928, 390);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 55);
+            this.label5.TabIndex = 28;
+            this.label5.Text = " ";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Image = global::miyagi.Properties.Resources.Verde;
+            this.label4.Location = new System.Drawing.Point(670, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 55);
+            this.label4.TabIndex = 27;
+            this.label4.Text = " ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Image = global::miyagi.Properties.Resources.Amarelo;
+            this.label3.Location = new System.Drawing.Point(413, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 55);
+            this.label3.TabIndex = 26;
+            this.label3.Text = " ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = global::miyagi.Properties.Resources.red;
+            this.label2.Location = new System.Drawing.Point(142, 390);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 55);
+            this.label2.TabIndex = 25;
+            this.label2.Text = " ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::miyagi.Properties.Resources.laranja;
+            this.pictureBox5.Location = new System.Drawing.Point(1081, 381);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(249, 73);
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::miyagi.Properties.Resources.Azul;
+            this.pictureBox4.Location = new System.Drawing.Point(824, 381);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(249, 73);
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::miyagi.Properties.Resources.Verde;
+            this.pictureBox3.Location = new System.Drawing.Point(567, 381);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(249, 73);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::miyagi.Properties.Resources.Amarelo;
+            this.pictureBox2.Location = new System.Drawing.Point(309, 381);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(249, 73);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::miyagi.Properties.Resources.vermelho;
+            this.pictureBox1.Location = new System.Drawing.Point(52, 381);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 73);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1354, 587);
+            this.Controls.Add(this.maskedTextBox5);
+            this.Controls.Add(this.maskedTextBox4);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "kata ティアゴ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox5;
+    }
+}
+
---
+++ miyagi/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace miyagi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {  
+            
+         if (keyData == (Keys.Space) || keyData == (Keys.Enter))
+         {
+            SendKeys.Send("{TAB}");
+         }
+
+           return base.ProcessCmdKey(ref msg, keyData);
+        }     
+
+    
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void somaResultadoJuri()
+        {
+            var textNota1 = Convert.ToDouble(label2.Text);
+
+            var textNota2 = Convert.ToDouble(label3.Text);
+
+            var textNota3 = Convert.ToDouble(label4.Text);
+
+            var textNota4 = Convert.ToDouble(label5.Text);
+
+            var textNota5 = Convert.ToDouble(label6.Text);
+
+            var notasReunidas = new double[] { textNota1, textNota2, textNota3, textNota4, textNota5 };
+
+            var PrimeiraNotaMaior = notasReunidas.OrderByDescending(x => x).Distinct().First();
+            var SegundaNotaMaior = notasReunidas.OrderByDescending(x => x).Distinct().Skip(1).FirstOrDefault();
+
+            var notaUm = notasReunidas.OrderByDescending(x => x).Distinct().Skip(2).FirstOrDefault();
+            var notaDois = notasReunidas.OrderByDescending(x => x).Distinct().Skip(3).FirstOrDefault();
+            var notaTres = notasReunidas.OrderByDescending(x => x).Distinct().Skip(4).FirstOrDefault();
+
+            var bmp = new Bitmap(miyagi.Properties.Resources.X);
+
+            Image imagemLabel = bmp;
+
+            var soma = notaUm + notaDois + notaTres;
+
+            var resultado = soma / 3;
+
+            var lbResultado = Convert.ToDouble(label1.Text);
+
+            lbResultado = Math.Round(resultado, 1);
+
+            label1.Text = lbResultado.ToString();
+
+            if (label2.Text == PrimeiraNotaMaior.ToString() || label2.Text == SegundaNotaMaior.ToString())
+            {
+                //Font FonteAlterada = new Font(label2.Font, FontStyle.Bold | FontStyle.Strikeout);
+
+                label2.Image = imagemLabel;
+            }
+
+            if (label3.Text == PrimeiraNotaMaior.ToString() || label3.Text == SegundaNotaMaior.ToString())
+            {
+                //Font FonteAlterada = new Font(label3.Font, FontStyle.Bold | FontStyle.Strikeout);
+
+                label3.Image = imagemLabel;
+            }
+
+            if (label4.Text == PrimeiraNotaMaior.ToString() || label4.Text == SegundaNotaMaior.ToString())
+            {
+               // Font FonteAlterada = new Font(label4.Font, FontStyle.Bold | FontStyle.Strikeout);
+
+                label4.Image = imagemLabel;
+            }
+
+            if (label5.Text == PrimeiraNotaMaior.ToString() || label5.Text == SegundaNotaMaior.ToString())
+            {
+               // Font FonteAlterada = new Font(label5.Font, FontStyle.Bold | FontStyle.Strikeout);
+
+                label5.Image = imagemLabel;
+            }
+
+            if (label6.Text == PrimeiraNotaMaior.ToString() || label6.Text == SegundaNotaMaior.ToString())
+            {
+                //Font FonteAlterada = new Font(label6.Font, FontStyle.Bold | FontStyle.Strikeout);
+
+                label6.Image = imagemLabel;
+            }
+        }
+
+        private void maskedTextBox1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space || e.KeyCode == (Keys.Enter))
+            {
+
+                label2.Text = maskedTextBox1.Text;
+                //somaResultadoJuri();
+                maskedTextBox1.Text = string.Empty;
+            }
+        }
+
+        private void maskedTextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space || e.KeyCode == (Keys.Enter))
+            {
+                label3.Text = maskedTextBox2.Text;
+               // somaResultadoJuri();
+                maskedTextBox2.Text = string.Empty;
+            }
+        }
+
+        private void maskedTextBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space || e.KeyCode == (Keys.Enter))
+            {
+                label4.Text = maskedTextBox3.Text;
+               // somaResultadoJuri();
+                maskedTextBox3.Text = string.Empty;
+            }
+        }
+
+        private void maskedTextBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space || e.KeyCode == (Keys.Enter))
+            {
+                label5.Text = maskedTextBox4.Text;
+               // somaResultadoJuri();
+                maskedTextBox4.Text = string.Empty;
+            }
+        }
+
+        private void maskedTextBox5_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space || e.KeyCode == (Keys.Enter))
+            {
+                label6.Text = maskedTextBox5.Text;
+                somaResultadoJuri();
+                maskedTextBox5.Text = string.Empty;
+            }
+        }
+
+        private void maskedTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //if (e.KeyChar == '.' || e.KeyChar == ',')
+            //{
+
+            //    //troca o . pela virgula
+            //    e.KeyChar = ',';
+
+            //    //Verifica se já existe alguma vírgula na string
+            //    if (maskedTextBox1.Text.Contains(","))
+            //    {
+            //        e.Handled = true; // Caso exista, aborte 
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("este campo aceita somente uma virgula");
+            //    }
+            //}
+
+            ////aceita apenas números, tecla backspace.
+            //else if (!char.IsNumber(e.KeyChar) && !(e.KeyChar == (char)Keys.Back))
+            //{
+            //    e.Handled = true;
+            //}
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
